b392e3a4b7792561dbba924763e62a937b6edb7f
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-09 12:35:45 +0200

    move into own class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        JavaBasics_3.main(args);
 
         // Java Basics - Data-Types Exercise
         JavaBasics_1.main(args);
 
         // Java Basics - Data-Types Exercise
         JavaBasics_2.main(args);
-    }
+
 }
 
