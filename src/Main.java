4ae2b99ccdea16a0a57e8948fe5c0fae8e3b2af5
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-09 12:30:48 +0200

    rebase to main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         // Java Basics - Data-Types Exercise
+        JavaBasics_1.main(args);
+
+        // Java Basics - Data-Types Exercise
         JavaBasics_2.main(args);
     }
 }
