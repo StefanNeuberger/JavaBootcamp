98a6f2343d414297c6ddd793ed5febcb0c4a4c67
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-09 12:49:41 +0200

    rebase

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Java Basics - Main Method Exercise
+        JavaBasics_1.main(args);
+
+        // Java Basics - Data-Types Exercise
+        JavaBasics_2.main(args);
+
+        // Java Basics - Operators Exercise
+        JavaBasics_3.main(args);
+
+        // Java Basics - If-Else Exercise
         JavaBasics_4.main(args);
     }
 }
